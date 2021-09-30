ba7ad9755ee22e514cb59ede86d7f28b24e954c3
EduCR03 <eduardocostar03@gmail.com>
2021-09-30 19:56:30 -0300

    Lista_3
    Nao sei se enviei do jeito certo no GitHub

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -8,20 +8,28 @@ public class Principal {
         int cpf;
         int qntEnd;
 
+        System.out.println("Entre com seus dados pessoais");
+        System.out.println("Digite o nome: ");
         nome = sc.nextLine();
+        System.out.println("Digite seu CPF: ");
         cpf = sc.nextInt();
+        System.out.println("Quantos enderecos o(a) senhor(a) tem: ");
         qntEnd = sc.nextInt();
 
         sc.nextLine();
 
         Pessoa p1 = new Pessoa(nome, cpf, qntEnd);
+        System.out.println("Entre com os dados de seu(s) endereco(s)");
         for (int i = 0; i < qntEnd; i++) {
             String bairro;
             String rua;
             int num;
-
+            System.out.println("Endereco " + i);
+            System.out.println("Bairro: ");
             bairro = sc.nextLine();
+            System.out.println("Rua: ");
             rua = sc.nextLine();
+            System.out.println("Numero: ");
             num = sc.nextInt();
             sc.nextLine();
 
